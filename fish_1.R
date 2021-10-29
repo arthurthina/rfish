fe5f1ef853dfbbb2d54e0567245a27fe3dbb8122
Arthur Thina <arthur.thina@gmail.com>
2021-10-28 22:19:41 -0300

    initial commit

---
+++ fish_1.R
@@ -0,0 +1,12 @@
+library(rfishdraw)
+library(ggplot2)
+
+file <- system.file("extdata", package = "rfishdraw")
+dat <- readRDS(paste0(file, "/", "output_json.rds"))
+for (i in seq(length(dat))) {
+  dat[[i]] <- cbind(dat[[i]],i)
+}
+plot(NA, xlim = c(0,520),ylim = c(-320,0), axes = F, xlab = " ", ylab = " ")
+for(i in seq(length(dat))) {
+  lines(x = dat[[i]][,1],y = -dat[[i]][,2], lwd = 1, col = "red")
+}
